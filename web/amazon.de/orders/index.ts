abf36ee59be1fd34cc96c397dd77178d1996f11b
Ralf Vogler <ralf.vogler@gmail.com>
2019-08-06 20:56:16 +0200

    check url to login/2fa as needed

--- web/amazon.de/orders/index.ts
+++ web/amazon.de/orders/index.ts
@@ -2,6 +2,9 @@ import puppeteer from 'puppeteer';
 import { submit } from '../../../util/puppeteer';
 import prompts from 'prompts';
 import validator from 'validator';
+import assert from 'assert';
+
+const target = 'https://www.amazon.de/gp/css/order-history';
 
 const main = async () => {
   const auth = await prompts([
@@ -11,23 +14,31 @@ const main = async () => {
   if (!auth.email || !auth.password) return;
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://www.amazon.de/gp/css/order-history');
+  await page.goto(target);
 
-  // login page
-  await page.type('[name=email]', auth.email);
-  await page.type('[name=password]', auth.password);
-  await page.click('[name=rememberMe]');
-  // await page.screenshot({ path: '1.png' });
-  await submit(page);
-
-  // 2FA page
-  if (auth.otp.length) {
-    await page.type('[name=otpCode]', auth.otp);
-    await page.click('[name=rememberDevice]');
+  if (page.url().startsWith('https://www.amazon.de/ap/signin')) {
+    // login page
+    await page.type('[name=email]', auth.email);
+    await page.type('[name=password]', auth.password);
+    await page.click('[name=rememberMe]');
+    // await page.screenshot({ path: '1.png' });
     await submit(page);
-    await page.waitFor(5000);
+    
+    // 2FA page
+    if (page.url().startsWith('https://www.amazon.de/ap/mfa')) {
+      if (!auth.otp.length) {
+        console.error('Got redirected to 2FA page, but you did not enter a code.');
+        return;
+      }
+      await page.type('[name=otpCode]', auth.otp);
+      await page.click('[name=rememberDevice]');
+      await submit(page);
+    }
   }
-
-  console.log(await page.cookies());
+  console.log(page.url());
+  assert(page.url().startsWith(target));
+  await page.waitFor(5000);
+  // console.log(await page.cookies());
+  // browser.close();
 };
 main();
