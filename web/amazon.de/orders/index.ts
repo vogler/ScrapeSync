43e2efb87f217c1354fc7e97ca7dd8edcd74b657
Ralf Vogler <ralf.vogler@gmail.com>
2019-08-08 21:12:32 +0200

    amazon orders, this querySelector API is annoying...

--- web/amazon.de/orders/index.ts
+++ web/amazon.de/orders/index.ts
@@ -4,13 +4,13 @@ import auth from '../../../util/auth'
 import puppeteer from 'puppeteer';
 import { submit } from '../../../util/puppeteer';
 
-const target = 'https://www.amazon.de/gp/css/order-history';
+const target = 'https://www.amazon.de/gp/your-account/order-history?orderFilter=year-2019';
 
 const main = async () => {
-  const browser = await puppeteer.launch({ userDataDir: "./user_data", headless: false, defaultViewport: null });
+  const browser = await puppeteer.launch({ userDataDir: "./user_data", headless: true, defaultViewport: null });
   const page = await browser.newPage();
   await page.goto(target);
-  
+
   // login page
   if (page.url().startsWith('https://www.amazon.de/ap/signin')) {
     const cred = await auth(target);
@@ -39,7 +39,31 @@ const main = async () => {
     await cred.save();
   }
   assert(page.url().startsWith(target));
-  await page.waitFor(5000);
-  // browser.close();
+  // const val = (e: Element | null) => e && e.innerHTML.trim() || ''; // can't pass functions into eval?
+  const orders = await Promise.all((await page.$$('div.order')).map(async div => {
+    const info = await div.$$eval('span.value', es => es.map(e => e.innerHTML.trim()));
+    const shipments = await div.$$eval('div.shipment', es => es.map(e => ({
+      status: (e => e && e.innerText.trim())(e.querySelector('span')),
+      items: Array.from(e.querySelectorAll('div.a-fixed-left-grid-inner')).map(e => {
+        const a = Array.from(e.querySelectorAll('a'));
+        return {
+          name: a[1].innerText,
+          url: a[1].href,
+          img: e.querySelector('img')!.getAttribute('data-a-hires'),
+          price: e.querySelector('nobr') && e.querySelector('nobr')!.innerHTML || e.querySelector('span.a-color-price')!.innerHTML.trim(),
+        };
+      })
+    })));
+    return {
+      order_date: info[0],
+      sum: info[1],
+      nr: info[2],
+      shipments,
+    };
+
+  }));
+  console.dir(orders, { depth: null });
+  // await page.waitFor(5000);
+  browser.close();
 };
 main();
